47c637f2c016a6a0f51d850790b7cab5233b24ad
lucas-pvg <lucaspvg1@gmail.com>
2022-11-23 20:43:38 -0300

    📝 Some anotations form the webdev coursa

---
+++ js/anotations.js
@@ -0,0 +1,22 @@
+// document.querySelector();
+// document.getElementsByTagName();
+// document.getElementsByClassName();
+// document.getElementById();
+
+
+// var navbarList = document.querySelector(".navbar li");
+
+// navbarList.classList.add("navbar-active");
+// navbarList.classList.remove("navbar-active");
+// navbarList.classList.toggle("navbar-active");
+
+
+// When dealing with text, using innerHTML changes everything inside the text tag (including another tags)
+// while using textContent changes only the text
+
+
+// var navbarA = document.querySelector(".navbar a");
+
+// navbarA.attributes;
+// navbarA.getAttribute("href");
+// navbarA.setAttribute("href", "www.google.com");
